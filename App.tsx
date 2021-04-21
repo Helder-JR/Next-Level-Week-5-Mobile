2ce9cd072bd8a482ee6a66066bc0d1065e6c49f7
Helder-JR <helder.junior@ccc.ufcg.edu.br>
2021-04-21 03:03:16 -0300

    Alterado o componente principal da aplicação para que carregue as páginas a partir do módulo de rotas.

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import AppLoading from 'expo-app-loading';
 import { useFonts, Jost_400Regular, Jost_600SemiBold } from '@expo-google-fonts/jost';
 
-import Welcome from './src/pages/Welcome';
+import Routes from './src/routes';
 
 export default function App() {
 	const [ fontsLoaded ] = useFonts({ Jost_400Regular, Jost_600SemiBold });
@@ -10,5 +10,5 @@ export default function App() {
 	if (!fontsLoaded)
 		return <AppLoading/>
 
-	return <Welcome/>
+	return <Routes/>
 }
